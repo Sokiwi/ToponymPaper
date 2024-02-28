18684e3dd54bc2d604017d36bef9aadf82c368ce
Søren Wichmann <wichmannsoeren@gmail.com>
2024-02-28 15:41:39 +0100

    Update code_for_toponym_paper.R

--- code_for_toponym_paper.R
+++ code_for_toponym_paper.R
@@ -1,10 +1,10 @@
-## SECTION 1. Introduction
+## General Introduction
 
 # Figure 1
 top("ham$", c("GB", "IE"))
 
 
-## SECTION 2. The GeoNames data
+## Methodology
 
 # Figure 2 and info on GeoNames
 library(RColorBrewer)
@@ -58,61 +58,7 @@ x_redux <- x[x$dem > -5000,]  # exclude NA values encoded as dem = -9999
 cor.test(x_redux$dem, x_redux$elevation)
 
 
-## SECTION 3. A practical introduction to the R package toponym
-
-# Installation
-install.packages("devtools")  # installs package “devtools”
-library ("devtools")  # loads package “devtools”
-install_github("Lennart05/toponym")  # installs package “toponym”
-library("toponym")  # loads package “toponym”
-
-# Create a simple map
-country("DE")
-country("country table")
-help(regex)  # to get an introduction to regular expressions
-top("ham$", c("GB", "IE"))  # creates Figure 1
-# verify that the name Seaham but not Walthamstow are among the hits
-sort(data_ham$name)
-top("ham", c("GB", "IE"))  # remove the dollar sign
-# verify that both of the names Seaham and Walthamstow are among the hits
-grep("Seaham", data_ham$name)
-grep("Walthamstow", data_ham$name)
-top("ham$", "IE")  # trailing string -ham in Ireland only
-
-# More on the top() function
-top(countries=c("GB", "IE"), strings="ham$")  # other order of parameters
-help(top)  # get info on the top() function
-top("ham$", c("GB", "IE"), color="blue", regions=1, csv=TRUE)
-top(strings="^Влад", countries="RU")
-top(strings="^Vlad", countries="RU", column="alternatenames")
-
-# Finding frequent toponym strings
-topFreq(countries="US", len=5, limit=24, type="$", feat.class="H")
-topFreq(countries="BE", len=5, type="^", limit=8, polygon=flanders_polygon)
-
-# Defining a polygon
-p <- createPolygon("ID")  # this will require defining a polygon through clicks
-country("MX", regions = 1)
-Chi <- createPolygon(countries="MX", region_name="Chihuahua", retrieve=TRUE)
-
-# Strings specific to a region
-topComp(countries="GB", len=2, rat=.7, limit=100, polygon=danelaw_polygon)
-topCompOut(countries="GB", len=2, rat=.7, limit=100, polygon=danelaw_polygon)
-# exloring cases of -thorpe
-length(data_pe$name)
-length(grep("thorpe", data_pe$name))
-thorpe <- data_pe[grep("thorpe", data_pe$name),]
-mapper(thorpe)
-topComp(countries="GB", limit=100, len=5, rat=.5, polygon=danelaw_polygon)
-topZtest(strings="stead$", countries="GB",polygon=danelaw_polygon)
-
-# Other functions
-getData(countries=c("DE", "GB"))  # example of updating downloaded data
-# examples of lists of symbols and their frequencies, here for Thailand
-ortho(countries="TH", column="name")  # 
-ortho(countries="TH", column="alternatenames")  # 
-
-## SECTION 4. Case study 1: tracking the former extension of Xincan languages
+## Tracking the former extent of Xincan languages
 # Mapping names mentioned in Thompson (1970: 98-99)
 thompson=c("Toquegua", "Motagua", "Managua", "Tigua", "Chanmagua", "Chaumagua", 
            "Jagua", "Pasasagua", "Anchagua", "Cocuyagua", "Jacagua", "Jalagua", 
@@ -187,7 +133,7 @@ top(strings=campbell_agua, countries=c("GT", "HN", "SV", "NI"),
 mapper(mapdata=campbell_agua, title="Names in -(a)gua/-ahua from Campbell")
 
 
-## SECTION 5. Case study 2: Slavic toponyms in Germany
+## Slavic toponyms in Germany
 # Figure 7
 top("itz$", "DE", regions=1)
 
